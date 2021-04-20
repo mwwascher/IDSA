feba2568d088ef4c5ed59896b6763b8ab5af518e
mwwascher <70895649+mwwascher@users.noreply.github.com>
2021-04-20 16:16:10 -0400

    Add files via upload

--- IDSA_open.R
+++ IDSA_open.R
@@ -1,11 +1,11 @@
 library(plotly)
 
-#####Simulate the epidemic
+#####Initialize
 n = 10000
 n.off = 30000
 n.days = 60
 n.tests = 1500
-set.seed(123)
+set.seed(789)
 
 test.mat = matrix(NA, nrow = n, ncol = 2)
 prop.mat = matrix(NA, nrow = n.days, ncol = 2)
@@ -13,7 +13,7 @@ prop.mat = matrix(NA, nrow = n.days, ncol = 2)
 beta.t = rep(.12, n.days)
 beta.t[1:7] = .35
 beta.t[8:14] = .45
-beta.t[15:21] = .08
+beta.t[14:21] = .2
 beta.t[22:28] = .08
 trace.prob = .85
 gamma.t = rep(.25, n.days)
@@ -73,6 +73,7 @@ R.count.off = c()
 trace.count.off = c()
 pos.count = rep(0, n.days-1)
 
+####Simulate the epidemic
 for (i in 1:(n.days-1))
 {
   
@@ -181,11 +182,11 @@ for (i in 1:(n.days-1))
 }
 
 #####Take a look at the epidemic
-plot(S.count, main = "Susceptibles", xlab = "day", ylab = "S")
-plot(I.count, main = "Infecteds", xlab = "day", ylab = "I")
-plot(S.count.off, main = "Susceptibles", xlab = "day", ylab = "S, off campus")
-plot(I.count.off, main = "Infecteds", xlab = "day", ylab = "I, off campus", ylim = c(0,900))
-points(I.count, col = "red")
+# plot(S.count, main = "Susceptibles", xlab = "day", ylab = "S")
+# plot(I.count, main = "Infecteds", xlab = "day", ylab = "I")
+# plot(S.count.off, main = "Susceptibles", xlab = "day", ylab = "S, off campus")
+# plot(I.count.off, main = "Infecteds", xlab = "day", ylab = "I, off campus", ylim = c(0,900))
+# points(I.count, col = "red")
 
 
 #####Build the test matrix
@@ -213,6 +214,7 @@ for (i in 1:(n.days-1))
   prop.mat[i,1] = prop.neg
 }
 
+#####Naive background prevlance estimate
 prev.off = rep(NA, n.days)
 for (i in 1:(n.days-1))
 {
@@ -306,6 +308,107 @@ S.comp.group = function(S, S.0, t)
   return(S.E)
 }
 
+loglik.eps = function(eps, delta, I, S)
+{
+  loglik = 0
+  lambda = S*psi.1/n
+  loglik = loglik + log(lambda[1]^delta[1]*exp(-lambda[1])/factorial(delta[1]))
+  for (i in 2:n.days)
+  {
+    loglik = loglik + log(lambda[i]^delta[i]*exp(-lambda[i])/factorial(delta[i]))
+  }
+  return(loglik)
+}
+
+IDSAplot_open = function()
+{
+  
+  write.csv(I.samp[(.5*nreps+1):nreps,], file = "posterior_I1.csv")
+  write.csv(S.samp[(.5*nreps+1):nreps,], file = "posterior_S1.csv")
+  write.csv(delta.samp[(.5*nreps+1):nreps,], file = "posterior_delta1.csv")
+  write.csv(eps.samp[(.5*nreps+1):nreps,], file = "posterior_eps1.csv")
+  write.csv(psi.1.samp[(.5*nreps+1):nreps,], file = "posterior_phi1.csv")
+  write.csv(gamma.samp[(.5*nreps+1):nreps,], file = "posterior_gamma1.csv")
+  
+  I.med = apply(I.samp[(.5*nreps+1):nreps,], 2, median)
+  I.max = apply(I.samp[(.5*nreps+1):nreps,], 2, quantile, probs = .975)
+  I.min = apply(I.samp[(.5*nreps+1):nreps,], 2, quantile, probs = .025)
+  
+  S.t.med = apply(S.t.samp[(.5*nreps+1):nreps,], 2, median)
+  
+  phi.med = apply(psi.1.samp[(.5*nreps+1):nreps,], 2, median)
+  
+  fig.off <- plot_ly(type = "scatter", mode = "lines", showlegend = TRUE)
+  fig.off <- fig.off %>% add_trace(x = c(1:(n.days-4)), y = I.min[1:(n.days-4)], mode = "lines", name = '2.5%', line = list(color = "rgb(44, 160, 44)"))
+  fig.off <- fig.off %>% add_trace(x = c(1:(n.days-4)), y = I.med[1:(n.days-4)], mode = "lines", name = '50%', line = list(color = "rgb(255, 127, 14)"), fill = "tonexty", fillcolor = "rgba(168, 216, 234, 0.5)")
+  fig.off <- fig.off %>% add_trace(x = c(1:(n.days-4)), y = I.max[1:(n.days-4)], mode = "lines", name = '97.5%', line = list(color = "rgb(214, 39, 40)"), fill = "tonexty", fillcolor = "rgba(168, 216, 234, 0.5)")
+  fig.off <- fig.off  %>% add_trace(x = c(1:(n.days-4)), y = I.count[1:(n.days-4)], mode = "markers", name = 'True prevalence')
+  fig.off <- fig.off  %>% add_segments(x = 8, xend = 8, y = 0, yend = 1000, name = TeX("\\beta_t \\text{ changepoint}"), line = list(color = 'rgb(12, 12, 24)', width = 2, dash = 'dot'), showlegend = FALSE)
+  fig.off <- fig.off  %>% add_segments(x = 15, xend = 15, y = 0, yend = 1000, name = "chpt 2", line = list(color = 'rgb(12, 12, 24)', width = 2, dash = 'dot'), showlegend = FALSE)
+  fig.off <- fig.off  %>% add_segments(x = 29, xend = 29, y = 0, yend = 1000, name = "chpt 1", line = list(color = 'rgb(12, 12, 24)', width = 2, dash = 'dot'), showlegend = FALSE)
+  fig.off <- fig.off  %>% add_segments(x = 22, xend = 22, y = 0, yend = 1000, name = "chpt 3", line = list(color = 'rgb(12, 12, 24)', width = 2, dash = 'dot'), showlegend = FALSE)
+  
+  t <- list(
+    family = "arial",
+    size = 22,
+    color = "black")
+  
+  t.2 <- list(
+    family = "arial",
+    size = 24,
+    color = "black")
+  
+  fig.off <- fig.off %>% layout(title = "", legend = list(x=.75, y=.95, font = t.2), xaxis = list(title = "Day", tickfont = list(size = 12)),
+                                yaxis = list(title = "Infected", range = c(0,660)), font = t,
+                                margin = list(b = 100))
+  fig.off <- fig.off %>% config(mathjax = 'cdn')
+  orca(fig.off, "Open_fit.svg")
+  
+  
+  #####R_t
+  gamma.true = pos.count/I.count
+  true.Rt = ((beta.t[1:59]*I.count) + (beta.cross[1:59]*I.count.off))/(gamma.true)*(S.count/I.count)/n
+  true.Rt[1:2] = ((beta.t[1:2]*I.count[1:2]) + (beta.cross[1:2]*I.count.off[1:2]))/(c(1/6,1/6))*(S.count[1:2]/I.count[1:2])/n
+  true.Rt.smooth = supsmu(c(2:60), true.Rt)
+  
+  #R.0.samp = (psi.1.samp[(.5*nreps+1):nreps,1:59]*I.samp[(.5*nreps+1):nreps,1:59]/n)/gamma.samp[(.5*nreps+1):nreps,1:59]*(S.t.samp[(.5*nreps+1):nreps,1:59]/n)
+  #R.0.samp = psi.1.samp[(.5*nreps+1):nreps,1:59]/gamma.samp[(.5*nreps+1):nreps,1:59]*(I.samp[(.5*nreps+1):nreps,1:59]*S.t.samp[(.5*nreps+1):nreps,1:59])/(n*n)
+  R.0.samp = (psi.1.samp[(.5*nreps+1):nreps,1:59]/gamma.samp[(.5*nreps+1):nreps,1:59])*(S.t.samp[(.5*nreps+1):nreps,1:59]/I.samp[(.5*nreps+1):nreps,1:59])/n
+  R.t.min = apply(R.0.samp, 2, quantile, probs = .025)
+  R.t.q1 = apply(R.0.samp, 2, quantile, probs = .25)
+  R.t.med = apply(R.0.samp, 2, quantile, probs = .5)
+  R.t.q3 = apply(R.0.samp, 2, quantile, probs = .75)
+  R.t.max = apply(R.0.samp, 2, quantile, probs = .975)
+  
+  # R.t.min = supsmu(c(1:59), R.t.min)
+  # R.t.min = R.t.min$y
+  # R.t.med = supsmu(c(1:59), R.t.med)
+  # R.t.med = R.t.med$y
+  # R.t.max = supsmu(c(1:59), R.t.max)
+  # R.t.max = R.t.max$y
+  
+  fig.rt <- plot_ly(type = "scatter", mode = "lines", showlegend = TRUE)
+  fig.rt <- fig.rt %>% add_trace(x = c(1:(n.days-4)), y = R.t.min[1:(n.days-4)], mode = "lines", name = '2.5%', line = list(color = "rgb(44, 160, 44)"))
+  fig.rt <- fig.rt %>% add_trace(x = c(1:(n.days-4)), y = R.t.med[1:(n.days-4)], mode = "lines", name = '50%', line = list(color = "rgb(255, 127, 14)"), fill = "tonexty", fillcolor = "rgba(168, 216, 234, 0.5)")
+  fig.rt <- fig.rt %>% add_trace(x = c(1:(n.days-4)), y = R.t.max[1:(n.days-4)], mode = "lines", name = '97.5%', line = list(color = "rgb(214, 39, 40)"), fill = "tonexty", fillcolor = "rgba(168, 216, 234, 0.5)")
+  fig.rt <- fig.rt %>% add_trace(x = c(1:(n.days-4)), y = true.Rt.smooth$y[1:(n.days-4)], mode = "lines", name = 'True partial R_t')
+  # fig.rt <- fig.rt %>% add_trace(x = c(2:(n.days-4)), y = phi.med[2:(n.days-4)]/5, mode = "lines", name = 'phi/5')
+  # fig.rt <- fig.rt %>% add_trace(x = c(2:(n.days-4)), y = I.med[2:(n.days-4)]/30, mode = "lines", name = 'I/30')
+  # fig.rt <- fig.rt %>% add_trace(x = c(2:(n.days-4)), y = phi.med[2:(n.days-4)]/5*I.med[2:(n.days-4)]/300, mode = "lines", name = 'phi*I')
+  #fig.rt <- fig.rt %>% add_trace(x = c(2:(n.days-4)), y = phi.med[2:(n.days-4)]/5*I.med[2:(n.days-4)]/300*S.t.med[2:(n.days-4)]/n, mode = "lines", name = 'phi*I*S/n')
+  fig.rt <- fig.rt  %>% add_segments(x = 8, xend = 8, y = 0, yend = 450, name = TeX("\\beta_t \\text{ changepoint}"), line = list(color = 'rgb(12, 12, 24)', width = 2, dash = 'dot'), showlegend = FALSE)
+  fig.rt <- fig.rt  %>% add_segments(x = 15, xend = 15, y = 0, yend = 450, name = "chpt 2", line = list(color = 'rgb(12, 12, 24)', width = 2, dash = 'dot'), showlegend = FALSE)
+  fig.rt <- fig.rt  %>% add_segments(x = 22, xend = 22, y = 0, yend = 450, name = "chpt 3", line = list(color = 'rgb(12, 12, 24)', width = 2, dash = 'dot'), showlegend = FALSE)
+  fig.rt <- fig.rt  %>% add_segments(x = 29, xend = 29, y = 0, yend = 450, name = "chpt 1", line = list(color = 'rgb(12, 12, 24)', width = 2, dash = 'dot'), showlegend = FALSE)
+  
+  fig.rt <- fig.rt %>% layout(title = "", legend = list(x=.75, y=.95, font = t.2), xaxis = list(title = "Day", tickfont = list(size = 12)),
+                              yaxis = list(title = "Partial R_t", range = c(0,6)), font = t,
+                              margin = list(b = 100))
+  fig.rt <- fig.rt %>% config(mathjax = 'cdn')
+  fig.rt
+  orca(fig.rt, "Open_Rt.svg")
+}
+
 #####Smooth background group I
 I.off = rep(0, n.days)
 for (i in 1:n.days)
@@ -315,23 +418,24 @@ for (i in 1:n.days)
 I.smoothed = smooth.spline(c(1:59), I.off[1:59], lambda = .0005)
 I.smoothed = trunc(I.smoothed$y)
 
-#####Model fit
-a.t = 1
-b.t = 3
+#####Model fit initialization
 inf.prop = (1-length(which(is.na(test.mat[,2])))/n)
 p.t = rep(inf.prop/n.days, n.days)
 S.0 = .99
 S.e.0 = .99
 p.t.null = rep(1/n.days, n.days)
 I = rep(0, n.days)
+I.new = rep(0, n.days)
 delta = rep(0, n.days)
 delta.off = rep(0, n.days)
 beta = rep(.4, n.days)
 eps = rep(0, n.days)
+eps.new = rep(0, n.days)
 eps.off = rep(0, n.days)
 omega = rep(0, n.days)
 omega.off = rep(0, n.days)
 gamma = rep(.2, n.days)
+gamma.new = rep(.166, n.days)
 psi.1 = rep(.4, n.days)
 psi.2 = rep(.4, n.days)
 phi = rep(.25, n.days)
@@ -342,7 +446,9 @@ S.t.off = rep(n.off, n.days)
 S.prod = rep(0, n.days)
 e.t = rep(0, n.days)
 e.t.off = rep(0, n.days)
-nreps = 2000
+nreps = 10000
+a = c(1,1, rep(10,58))
+b = c(9,9, rep(50, 58))
 
 S.samp = matrix(0, nrow = nreps, ncol = n.days)
 S.t.samp = matrix(0, nrow = nreps, ncol = n.days)
@@ -351,6 +457,7 @@ beta.off.samp = matrix(0, nrow = nreps, ncol = n.days)
 beta.cross.samp = matrix(0, nrow = nreps, ncol = n.days)
 gamma.samp = matrix(0, nrow = nreps, ncol = n.days)
 delta.samp = matrix(0, nrow = nreps, ncol = n.days)
+eps.samp = matrix(0, nrow = nreps, ncol = n.days)
 I.samp = matrix(0, nrow = nreps, ncol = n.days)
 e.samp = matrix(0, nrow = nreps, ncol = n.days)
 beta.track = matrix(0, nrow = 2, ncol = nreps)
@@ -359,24 +466,66 @@ psi.1.samp = matrix(0, nrow = nreps, ncol = n.days)
 decensor(test.mat[500,], p.t = p.t, S.0 = .99)
 inf.dates = as.vector(apply(test.mat[1:n,], 1, decensor, p.t=p.t, S.0 = .99))  
 ex.dates = inf.dates - rgeom(length(inf.dates), phi.t[1])-1
+ex.geom = inf.dates - ex.dates
+rec.dates = inf.dates + rgeom(length(inf.dates), 1/6)+1
+ex.rec = 0
+accept.eps = 0
+bad.ratio = 0
 
+accept = 0
+reject = 0
+
+#####Gibbs sampler
 for (i in 1:nreps)
   {
   X.unif = runif(1, -.003, .003)
-  I.0 = max(1, trunc(length(which(inf.dates == 0)) + n*X.unif))
-  E.0 = max(1, trunc(length(which(ex.dates <= 0 & inf.dates > 0)) + n*X.unif))
+  I.0 = trunc((length(R[[3]])/1500+X.unif)*n)
+  E.0 = trunc((length(R[[3]])/1500+X.unif)*n)
   delta[1] = length(which(ex.dates == 1))
   omega[1] = length(which(inf.dates == 1))
-  eps[1] = rbinom(1, I.0, gamma[1])
+  gamma[1] = rbeta(1,a[1],b[1])
+  gamma.new[1] = rbeta(1,a[1],b[1])
+  eps.new[1] = rbinom(1, I.0, gamma[1])
   eps.off[1] = rbinom(1,trunc(.01*n.off), gamma[1])
   I[1] = I.0 + omega[1] - eps[1]
+  I.new[1] = I.0 + omega[1] - eps.new[1]
   delta.off[1] = max(0, I.smoothed[1] + eps.off[1] - trunc(.01*n.off))
   e.t[1] = I.0 + delta[1] - omega[1]
   S.t[1] = n - I.0 - E.0 - delta[1]
   S.t.off[1] = .98*n.off - delta.off[1]
   psi.1[1] = rgamma(1, delta[1] + 1, S.t[1]/n*(1+1))
   psi.2[1] = rgamma(1, delta.off[1] + 1, S.t.off[1]/n.off*(1+1))
-  gamma[1] = rbeta(1,1,10)
+  
+  for(h in 2:n.days)
+  {
+    eps.new[h] = rbinom(1, I.new[h-1], gamma.new[h-1])
+    delta[h] = length(which(ex.dates == h))
+    omega[h] = length(which(inf.dates == h))
+    I[h] = I[h-1] + omega[h] - eps[h]
+    I.new[h] = I.new[h-1] + omega[h] - eps.new[h]
+    S.t[h] = S.t[h-1] - delta[h]
+    e.t[h] = e.t[h-1] + delta[h] - omega[h]
+    gamma.new[h] = rbeta(1, eps.new[h] + a[h], I.new[h-1] - eps.new[h] + b[h])
+  }
+  if (length(which(I <= 0)) > 0)
+  {
+    flip.eps = 1
+  } else {
+    ratio.eps = loglik.eps(eps.new, delta, I.new, S.t) - loglik.eps(eps, delta, I, S.t)
+    ratio.eps = min(1, exp(ratio.eps))
+    if (is.na(ratio.eps))
+    {
+      bad.ratio = bad.ratio + 1
+      ratio.eps = 1
+    }
+    flip.eps = rbinom(1,1,ratio.eps)
+  }
+  if (flip.eps == 1)
+  {
+    eps = eps.new
+    I = I.new
+    accept.eps = accept.eps + 1
+  }
   
   
   for (j in 2:(n.days-1))
@@ -384,21 +533,21 @@ for (i in 1:nreps)
     eps.off[j] = rbinom(1, I.smoothed[j-1], gamma.off[j])
     delta.off[j] = max(I.smoothed[j] - I.smoothed[j-1] + eps.off[j], 0)
     S.t.off[j] = S.t.off[j-1] - delta[j]
-    eps[j] = rbinom(1, I[j-1], gamma[j-1])
+    #eps[j] = rbinom(1, I[j-1], gamma[j-1])
     
-    delta[j] = length(which(ex.dates == j))
-    omega[j] = length(which(inf.dates == j))
-    I[j] = I[j-1] + omega[j] - eps[j]
-    e.t[j] = e.t[j-1] + delta[j] - omega[j]
-    S.t[j] = S.t[j-1] - delta[j]
+    #delta[j] = length(which(ex.dates == j))
+    #omega[j] = length(which(inf.dates == j))
+    #I[j] = I[j-1] + omega[j] - eps[j]
+    #e.t[j] = e.t[j-1] + delta[j] - omega[j]
+    #S.t[j] = S.t[j-1] - delta[j]
   }
   
   for (k in 2:n.days)
     {
-    psi.1[k] = rgamma(1, delta[k] + 1, S.t[k]/n*(1+1))
-    psi.2[k] = rgamma(1, delta.off[k] + 1, S.t.off[k]/n.off*(1+1))
-    gamma[k] = rbeta(1, eps[j] + 10, I[j-1] - eps[j] + 50)
-    gamma.off[k] = rbeta(1, eps.off[j] + a.t, I.smoothed[j-1] - eps.off[j] + b.t)
+    psi.1[k] = rgamma(1, delta[k] + .01, S.t[k]/n*(1+.01))
+    psi.2[k] = rgamma(1, delta.off[k] + .01, S.t.off[k]/n.off*(1+.01))
+    gamma[k] = rbeta(1, eps[j] + a[h], I[j-1] - eps[j] + b[h])
+    gamma.off[k] = rbeta(1, eps.off[j] + a[k], I.smoothed[j-1] - eps.off[j] + b[k])
   }
 
   
@@ -431,10 +580,48 @@ for (i in 1:nreps)
   
   
   p.t = p.fun(S.e, S.0)
-  p.t[p.t < 0] = 0
+  p.t.ex = p.fun(S, S.0)
+  p.t[p.t <= 0] = 1/n
+  p.t.ex[p.t.ex <= 0] = 1/n
   
   inf.dates = as.vector(apply(test.mat[1:n,], 1, decensor, p.t=p.t, S.0 = S.e.0))
-  ex.dates = inf.dates - rgeom(length(inf.dates), phi.t[1])-1
+  ex.dates = inf.dates - ex.geom
+  for (l in which(inf.dates > 0))
+  {
+    ex.date.new = inf.dates[l] - rgeom(1, phi.t[1]) - 1
+    if (is.na(ex.dates[l]))
+    {
+      a.ratio = 1
+    } else {
+      
+      if (ex.dates[l] <= 0 & ex.date.new <= 0)
+      {
+        a.ratio = 1
+      }
+      if (ex.dates[l] > 0 & ex.date.new <= 0)
+      {
+        a.ratio = min(1, (S.0*(1-S.0))/(S[ex.dates[l]]*p.t[ex.dates[l]]))
+      }
+      if (ex.dates[l] <= 0 & ex.date.new > 0)
+      {
+        a.ratio = min(1, (S[ex.date.new]*p.t.ex[ex.date.new])/(S.0*(1-S.0)))
+      }
+      if (ex.dates[l] > 0 & ex.date.new > 0)
+      {
+        a.ratio = min(1, (S[ex.date.new]*p.t.ex[ex.date.new])/(S[ex.dates[l]]*p.t.ex[ex.dates[l]]))
+      }
+    }
+    flip.ex = rbinom(1,1,a.ratio)
+    if (flip.ex == 1)
+    {
+      ex.dates[l] = ex.date.new
+      accept = accept + 1
+    } else {
+      reject = reject + 1
+    }
+    
+  }
+  ex.geom = inf.dates - ex.dates
   
   S.t.samp[i,] = S.t
   S.samp[i,] = S
@@ -442,59 +629,10 @@ for (i in 1:nreps)
   delta.samp[i,] = delta
   gamma.samp[i,] = gamma
   psi.1.samp[i,] = psi.1
+  eps.samp[i,] = eps
 }
 
 
 
 #####Prevalence
-I.med = apply(I.samp[1001:2000,], 2, median)
-I.max = apply(I.samp[1001:2000,], 2, quantile, probs = .975)
-I.min = apply(I.samp[1001:2000,], 2, quantile, probs = .025)
-
-fig.off <- plot_ly(type = "scatter", mode = "lines", showlegend = TRUE)
-fig.off <- fig.off %>% add_trace(x = c(1:(n.days-4)), y = I.med[1:(n.days-4)], mode = "lines", name = '50%')
-fig.off <- fig.off %>% add_trace(x = c(1:(n.days-4)), y = I.min[1:(n.days-4)], mode = "lines", name = '2.5%', fill = "tonexty", fillcolor = "rgba(168, 216, 234, 0.5)")
-fig.off <- fig.off %>% add_trace(x = c(1:(n.days-4)), y = I.max[1:(n.days-4)], mode = "lines", name = '97.5%', fill = "tonexty", fillcolor = "rgba(168, 216, 234, 0.5)")
-fig.off <- fig.off  %>% add_trace(x = c(1:(n.days-4)), y = I.count[1:(n.days-4)], mode = "markers", name = 'True prevalence')
-fig.off <- fig.off  %>% add_segments(x = 8, xend = 8, y = 0, yend = 450, name = TeX("\\beta_t \\text{ changepoint}"), line = list(color = 'rgb(12, 12, 24)', width = 2, dash = 'dot'))
-fig.off <- fig.off  %>% add_segments(x = 15, xend = 15, y = 0, yend = 450, name = "chpt 2", line = list(color = 'rgb(12, 12, 24)', width = 2, dash = 'dot'), showlegend = FALSE)
-fig.off <- fig.off  %>% add_segments(x = 29, xend = 29, y = 0, yend = 450, name = "chpt 1", line = list(color = 'rgb(12, 12, 24)', width = 2, dash = 'dot'), showlegend = FALSE)
-
-t <- list(
-  family = "arial",
-  size = 18,
-  color = "black")
-
-fig.off <- fig.off %>% layout(title = "", legend = list(x=.75, y=.9), xaxis = list(title = "Day", tickfont = list(size = 12)),
-                            yaxis = list(title = "Infected", range = c(0,500)), font = t,
-                            margin = list(b = 100))
-fig.off <- fig.off %>% config(mathjax = 'cdn')
-fig.off
-
-
-#####R_t
-true.Rt = ((beta.t[1:59]*I.count) + (beta.cross[1:59]*I.count.off))/(I.count)/(1/((I.count)/pos.count))*S.count/n
-true.Rt[1:2] = ((beta.t[1:2]*I.count[1:2]) + (beta.cross[1:2]*I.count.off[1:2]))/(I.count[1:2])/(1/6)*S.count[1:2]/n
-
-R.0.samp = (psi.1.samp[(.5*nreps+1):nreps,1:59]*I.samp[(.5*nreps+1):nreps,1:59]/n)/gamma.samp[(.5*nreps+1):nreps,1:59]*(S.t.samp[(.5*nreps+1):nreps,1:59]/n)
-R.0.samp = psi.1.samp[(.5*nreps+1):nreps,1:59]/gamma.samp[(.5*nreps+1):nreps,1:59]*(I.samp[(.5*nreps+1):nreps,1:59]*S.t.samp[(.5*nreps+1):nreps,1:59])/(n*n)
-R.t.min = apply(R.0.samp, 2, quantile, probs = .025)
-R.t.q1 = apply(R.0.samp, 2, quantile, probs = .25)
-R.t.med = apply(R.0.samp, 2, quantile, probs = .5)
-R.t.q3 = apply(R.0.samp, 2, quantile, probs = .75)
-R.t.max = apply(R.0.samp, 2, quantile, probs = .975)
-
-fig.rt <- plot_ly(type = "scatter", mode = "lines", showlegend = TRUE)
-fig.rt <- fig.rt %>% add_trace(x = c(2:(n.days-4)), y = R.t.med[2:(n.days-4)], mode = "lines", name = '50%')
-fig.rt <- fig.rt %>% add_trace(x = c(2:(n.days-4)), y = R.t.min[2:(n.days-4)], mode = "lines", name = '2.5%', fill = "tonexty", fillcolor = "rgba(168, 216, 234, 0.5)")
-fig.rt <- fig.rt %>% add_trace(x = c(2:(n.days-4)), y = R.t.max[2:(n.days-4)], mode = "lines", name = '97.5%', fill = "tonexty", fillcolor = "rgba(168, 216, 234, 0.5)")
-fig.rt <- fig.rt %>% add_trace(x = c(2:(n.days-4)), y = true.Rt[2:(n.days-4)], mode = "markers", name = 'True pseudo-R_t', fill = "tonexty", fillcolor = "rgba(168, 216, 234, 0.5)")
-fig.rt <- fig.rt  %>% add_segments(x = 8, xend = 8, y = 0, yend = 450, name = TeX("\\beta_t \\text{ changepoint}"), line = list(color = 'rgb(12, 12, 24)', width = 2, dash = 'dot'))
-fig.rt <- fig.rt  %>% add_segments(x = 15, xend = 15, y = 0, yend = 450, name = "chpt 2", line = list(color = 'rgb(12, 12, 24)', width = 2, dash = 'dot'), showlegend = FALSE)
-fig.rt <- fig.rt  %>% add_segments(x = 29, xend = 29, y = 0, yend = 450, name = "chpt 1", line = list(color = 'rgb(12, 12, 24)', width = 2, dash = 'dot'), showlegend = FALSE)
-
-fig.rt <- fig.rt %>% layout(title = "", legend = list(x=.75, y=.9), xaxis = list(title = "Day", tickfont = list(size = 12)),
-                            yaxis = list(title = "Partial R_t", range = c(0,14)), font = t,
-                            margin = list(b = 100))
-fig.rt <- fig.rt %>% config(mathjax = 'cdn')
-fig.rt
+IDSAplot_open()
